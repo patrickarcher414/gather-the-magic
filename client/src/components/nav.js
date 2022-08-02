8fbeabb7ae9d99bfb5c28fbeea526348dd938f60
Joshua Rangai <rangaij0116@hotmail.com>
2022-08-02 18:42:51 -0500

    home and logout icon

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,23 +1,21 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-
-
 const Nav = () => {
-
-
-
   return (
     <div className="container">
       <h1>Gather the Magic</h1>
       {/*  COPY FOR EACH PAGE */}
-      <Link to="/" >Dashboard</Link>
+      <div className="flex-right">
+        <Link to="/">
+          <i className="fab fa-solid fa-house fa-2x"></i>
+        </Link>
+        <Link to="/login">
+          <i className="fa-solid fa-person-running fa-2x"></i>
+        </Link>
+      </div>
     </div>
-  )
-   
-  
-}
-
-
+  );
+};
 
 export default Nav;
