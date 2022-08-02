0f58faca3633520d4ff48efd1293b92b75fd75e1
Joshua Rangai <rangaij0116@hotmail.com>
2022-08-02 14:37:22 -0500

    index

---
+++ client/src/components/Card.js/index.js
@@ -0,0 +1,17 @@
+function Card() {
+  //   const cardTitle = document.querySelectorAll(".card-title");
+  // fetch("URL").then(reponse => JSON.stringify).then(cardTiltle.innerHTML = {data.title})
+  return (
+    <div className="card" style={{ width: "18rem" }}>
+      <div className="card-body">
+        <h5 className="card-title">Card Title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
