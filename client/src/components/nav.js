796ab052ee5b89a9ea7ce86053a21ae8c9d315da
0ffpudding <jennaanderson00@gmail.com>
2022-08-04 13:57:27 -0500

    centered components in nav and cardsGallery, added animations and neon effects

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,23 +1,26 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-
-
 const Nav = () => {
-
-
-
   return (
-    <div className="container">
-      <h1>Gather the Magic</h1>
-      {/*  COPY FOR EACH PAGE */}
-      <Link to="/" >Dashboard</Link>
+    <div 
+      className="container"
+      id="navContainer"
+    >
+      <div id="navHeaderBox">
+        <h1 id="navHeader">Gather the Magic</h1>
+      </div>
+      <div id="navLinksBox">
+        {/*  COPY FOR EACH PAGE */}
+        <Link 
+          id="dashLink"
+          to="/" 
+        >
+          Dashboard
+        </Link>
+      </div>
     </div>
-  )
-   
-  
+  ) 
 }
-
-
 
 export default Nav;
