ffb6321623c12458f2fe39b56c8c8f5497e4ba25
Joshua Rangai <rangaij0116@hotmail.com>
2022-08-04 18:44:09 -0500

    connection db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // CHANGE PLACEHOLDER (DB_NAME) WHEN THE DATABASE IS NAMED
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/db_name', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/gather-the-magic",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
-module.exports = mongoose.connection
+module.exports = mongoose.connection;
