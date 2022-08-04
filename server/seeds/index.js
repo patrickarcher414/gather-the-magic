4a504cd22c65dbb4adfab60498568040bf9fea2b
Cameron <sicklecj@gmail.com>
2022-08-04 17:37:54 -0500

    seed testing

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -1,14 +1,16 @@
 const connection = require('../config/connection')
-const { User } = require('../models')
+const { User, Comment, Card } = require('../models')
 
 connection.on('open', async () => {
-  
+
   await User.deleteMany()
-  
+
   await User.create({
     username: 'Sosa',
     email: 'cheif.keef@email.com',
     password: 'supersecretpass'
   })
+
+
   process.exit(0)
 })
